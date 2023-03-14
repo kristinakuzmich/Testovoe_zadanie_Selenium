1b5ee845b6432ffee1134f874b7189bc69d42722
kuzmich <kristina.kuzmich.33@gmail.ru>
2023-03-14 12:52:03 +0300

    - Исправлен класс ConfProperties.java, добавлена переменная, которая указывает кодировку "UTF-8". - Изменено ожидание с Thread.sleep() на явное wait (класса WebDriverWait). Метода создания переменной перенесен в класс PageBaseClass. - Создан класс enum Messages, в котором хранятся сообщени я приложения. - В классе ReceptionOfDocsTestData повторяющиеся данные подаются через переменные. - Данные пользователя подаются из файла Conf.properties.

--- src/main/java/MainMenuPage.java
+++ src/main/java/MainMenuPage.java
@@ -1,11 +1,13 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 public class MainMenuPage extends PageBaseClass {
-    @FindBy(xpath = "//span[text()='Кузьмич М.И.']")
+    @FindBy(xpath = "//form[@id='footer-form']/div[@class='session']/span[1]")
     private WebElement userMenu;
     @FindBy(xpath = "//*[contains(@id, 'left-menu-form:tabView:treeFilterInput')]")
     private WebElement searchField;
-    @FindBy(xpath = "//span[contains(text(),'Прием по документам')]")
+    @FindBy(xpath = "//span[contains(text(),'РџСЂРёРµРј РїРѕ РґРѕРєСѓРјРµРЅС‚Р°Рј')]")
     private WebElement docField;
     @FindBy(xpath = "//span[@class='ui-growl-title']")
     private WebElement messText;
@@ -16,15 +18,17 @@ public class MainMenuPage extends PageBaseClass {
     }
     public void searchField(String value) {
         searchField.sendKeys(value);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='ajaxStatusPanel']/div[@style='display: none;']")));
     }
     public void clickDoc() {
         docField.click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='ajaxStatusPanel']/div[@style='display: none;']")));
     }
     public String getMessText() {
         return messText.getText();
     }
     public void exitCF() {
         exitBtn.click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("formLogin")));
     }
-
 }
