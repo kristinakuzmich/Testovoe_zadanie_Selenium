1bad77de1051a75f2cac9e79e34ae56cd4282350
kuzmich <kristina.kuzmich.33@gmail.ru>
2023-03-15 17:11:06 +0300

    Дополнен тест проверкой остатков на рм: проверяется изменение остатков после проведения операции.

--- src/main/java/MainMenuPage.java
+++ src/main/java/MainMenuPage.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 public class MainMenuPage extends PageBaseClass {
+    Expectation expectation=new Expectation();
     @FindBy(xpath = "//form[@id='footer-form']/div[@class='session']/span[1]")
     private WebElement userMenu;
     @FindBy(xpath = "//*[contains(@id, 'left-menu-form:tabView:treeFilterInput')]")
